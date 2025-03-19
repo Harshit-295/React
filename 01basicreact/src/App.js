adad9892abbebc8f0e326a5e2ed52f597ea11b50
Harshit-295 <bhardwajharsh1984@gmail.com>
2025-03-19 21:09:49 +0530

    01basic react learn about react flow and structure

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <h1>Chai aur react </h1>,
+    <p>This is my first vedio in which i learn about the react.. main key points i learn here are 
+      1.how to download react app using npx-create-react-app this is the first method we can dowload
+      the react app using vite by using vite we use abstarction method so that some of files are removed
+      make download easily </p>
+  );
+}
+
+export default App;
